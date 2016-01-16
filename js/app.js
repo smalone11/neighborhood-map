9bb354c8ecbad50aa6c19b96036caf8e1eaa186d
Stephen <thraw415@gmail.com>
2016-01-15 19:24:07 -0800

    Completed animation and info window functionality!

--- js/app.js
+++ js/app.js
@@ -4,31 +4,36 @@ var favorites = [
     'title': 'Safeco Field',
     'lat': 47.5916418,
     'lng': -122.3354621,
-    'zIndex': 1
+    'zIndex': 1,
+    'info': 'Safeco Field'
   },
   {
     'title': 'The Crumpet Shop',
     'lat': 47.6090057,
     'lng': -122.3403555,
-    'zIndex': 2
+    'zIndex': 2,
+    'info': 'The Crumpet Shop'
   },
   {
     'title': 'Seattle Art Museum',
     'lat': 47.606646,
     'lng': -122.3395729,
-    'zIndex': 3
+    'zIndex': 3,
+    'info': 'Seattle Art Museum'
   },
   {
     'title': 'Showbox SoDo',
     'lat': 47.5874092,
     'lng': -122.3344886,
-    'zIndex': 4
+    'zIndex': 4,
+    'info': 'Showbox SoDo'
   },
   {
     'title': 'GameWorks',
     'lat': 47.611404,
     'lng': -122.3354338,
-    'zIndex': 5
+    'zIndex': 5,
+    'info': 'GameWorks'
   }
 ];
 
@@ -64,30 +69,28 @@ var setMarkers = function (map, favList) {
     });
 
     var info = new google.maps.InfoWindow({
-      content: 'poop'
+      content: fav.info
     });
 
-    marker.addListener('click', function () {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function() {
-        marker.setAnimation(null);
-      }, 1400);
-    });
-
-    marker.addListener('click', function() {
-      info.open(map, marker);
-    });
+    markerExtras(map, marker, info);
 
     markers.push(marker);
   }
 }
 
-// Adds a bounce animation to corresponding marker
-function toggleBounce (mark) {
-  mark.setAnimation(google.maps.Animation.BOUNCE);
-  setTimeout(function() {
-    mark.setAnimation(null);
-  }, 1400);
+// Adds animation and info to a marker.
+var markerExtras = function (map, marker, info) {
+  marker.addListener('click', function () {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      marker.setAnimation(null);
+    }, 1400);
+  });
+
+  marker.addListener('click', function() {
+    info.open(map, marker);
+  });
+
 }
 
 // Sets the map on all markers in the array.
@@ -108,70 +111,75 @@ function deleteMarkers () {
   markers = [];
 }
 
+// Triggers animation and info window for marker.
+function toggleBounce (map, mark) {
+  google.maps.event.trigger(mark, 'click');
+}
+
 var ViewModel = function () {
   var self = this;
 
   self.favList = ko.observableArray([]);
   self.searchLocation = ko.observable('');
 
-  // Populate observable array from favorites
+  // Populate observable array from favorites.
   favorites.forEach(function (favInfo) {
     self.favList.push(favInfo);
   })
 
-  // Adds Yelp info and Displays info when list name or marker is clicked on
+  // Adds Yelp info and Displays info when list name or marker is clicked on.
 
 
-  // Add animation to markers
+  // Triggers animation and info window for marker when name of marker is clicked on
   self.bounce = function (mark) {
-    toggleBounce(markers[mark.zIndex]);
+    toggleBounce(map, markers[mark.zIndex]);
   }
 
-  // Filters list and markers based on user input in the search bar
+  // Filters list and markers based on user input in the search bar.
   self.searchLocation.subscribe(function (loc) {
 
-    // Runs code if the search bar is not blank
+    // Runs code if the search bar is not blank.
     if (loc !== '') {
       var locLength = loc.length;
 
-      // Clears observable array
+      // Clears observable array.
       self.favList.removeAll();
 
-      // Runs through each object in favorites to compare to user input
+      // Runs through each object in favorites to compare to user input.
       favorites.forEach(function (favInfo) {
         var favTitle = favInfo.title;
 
-        // Runs through each letter in the location name
+        // Runs through each letter in the location name.
         for (var i = 0; i < favTitle.length; i++) {
           var fav = '';
 
-          // Adds on additional letters to match the user input string length
+          // Adds on additional letters to match the user input string length.
           for (var j = 0; j < locLength; j++) {
             fav = fav + favTitle[i+j];
           }
 
-          // Runs if the location name's string matches the user input's string
+          // Runs if the location name's string matches the user input's string.
           if (fav.toLowerCase() == loc.toLowerCase()) {
-            // Adds location name to array and exits the loop for said location name
+            // Adds location name to array and exits the loop for said location name.
             self.favList.push(favInfo);
             return
           }
         }
       })
 
-      // Clears markers from google maps and adds the new arrays
+      // Clears markers from google maps and adds the new arrays.
       deleteMarkers();
       setMarkers(map, self.favList());
 
     }else {
-      // Clears observable array and then populates it with all of favorites' objects
+      // Clears observable array and then populates it with all of favorites' objects.
       self.favList.removeAll();
 
       favorites.forEach(function (favInfo) {
         self.favList.push(favInfo);
       })
 
-      // Clears markers from google maps and adds the new arrays
+      // Clears markers from google maps and adds the new arrays.
       deleteMarkers();
       setMarkers(map, self.favList());
     }
